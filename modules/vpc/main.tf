07cc52fb9f7485cc60d12f012ec2097de1b3349d
redhatyash <104993408+redhatyash@users.noreply.github.com>
2026-07-13 19:43:24 +0530

    Replace aws_security_group with aws_default_security_group
    Replaced aws_security_group with aws_default_security_group and updated security group settings.

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -44,26 +44,20 @@ resource "aws_vpc" "this" {
     var.tags
   )
 }
-resource "aws_security_group" "this" {
-  name   = "${var.name}-sg"
+resource "aws_default_security_group" "default" {
   vpc_id = aws_vpc.this.id
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
-  }
+  ingress = []
+  egress  = []
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = var.tags
+  tags = merge(
+    {
+      Name = "${var.name}-default-sg"
+    },
+    var.tags
+  )
 }
+
 resource "aws_flow_log" "example" {
   iam_role_arn    = "arn:aws:iam::123456789012:role/VPCFlowLogsRole"
   log_destination = "arn:aws:logs:ap-south-1:123456789012:log-group:flowlogs"
